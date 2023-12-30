88602e8bb337293d85d6d7c68519d9cede29316f
aureliedjied <aurelie.djied@gmail.com>
2023-12-30 22:20:29 +0100

    fixtures with faker load + relation comment and articles

--- config/bundles.php
+++ config/bundles.php
@@ -15,4 +15,5 @@ return [
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
+    Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
 ];
--- src/DataFixtures/ArticleFixtures.php
+++ src/DataFixtures/ArticleFixtures.php
@@ -2,10 +2,11 @@
 
 namespace App\DataFixtures\Article;
 
-use Doctrine\Bundle\FixturesBundle\Fixture;
-use Doctrine\Persistence\ObjectManager;
-use App\Entity\Article;
 use Faker\Factory;
+use App\Entity\Article;
+use App\Entity\Comment;
+use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
 
 class ArticleFixtures extends Fixture
 {
@@ -18,6 +19,27 @@ class ArticleFixtures extends Fixture
             $article->setTitle($faker->sentence);
             $article->setContent($faker->paragraph);
             $manager->persist($article);
+
+            $this->addReference("article_$i", $article);
+        }
+
+        $manager->flush();
+        $this->loadComment($manager);
+    }
+
+    public function loadComment(ObjectManager $manager)
+    {
+        $faker = Factory::create();
+
+        // Créer 20 commentaires factices
+        for ($i = 0; $i < 20; $i++) {
+            $comment = new Comment();
+            $comment->setAuthor($faker->name);
+            $comment->setContent($faker->paragraph);
+            // Associer le commentaire à un article existant (à adapter selon votre structure)
+            $comment->setArticle($this->getReference('article_' . $i));
+
+            $manager->persist($comment);
         }
 
         $manager->flush();
