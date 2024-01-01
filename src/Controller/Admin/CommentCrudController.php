890aedba1659656face40b70faa775531e5c3d6a
aureliedjied <aurelie.djied@gmail.com>
2024-01-01 21:12:14 +0100

    crud users with only email roles and name

--- src/Controller/Admin/CommentCrudController.php
+++ src/Controller/Admin/CommentCrudController.php
@@ -3,6 +3,9 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Comment;
+use EasyCorp\Bundle\EasyAdminBundle\Field\BooleanField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 
 class CommentCrudController extends AbstractCrudController
@@ -12,14 +15,23 @@ class CommentCrudController extends AbstractCrudController
         return Comment::class;
     }
 
-    /*
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
+            AssociationField::new('author', 'Auteur')
+                ->formatValue(function ($value, $entity) {
+                    // affichage de l'auteur en concaténant le nom et le prénom
+                    return $entity->getAuthor()->getFirstname() . ' ' . $entity->getAuthor()->getLastname();
+                })
+                ->setFormTypeOption('disabled', true), // Désactiver l'édition dans le formulaire
+            TextEditorField::new('content', 'Message'),
+            AssociationField::new('article', 'Article')
+                ->formatValue(function ($value, $entity) {
+                    // affichage du titre de l'article
+                    return $entity->getArticle()->getTitle();
+                }),
+            BooleanField::new('isApproved', 'Approuvé/Désapprouvé')
+
         ];
     }
-    */
 }
--- src/Controller/Admin/UserCrudController.php
+++ src/Controller/Admin/UserCrudController.php
@@ -3,6 +3,9 @@
 namespace App\Controller\Admin;
 
 use App\Entity\User;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\EmailField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ChoiceField;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 
 class UserCrudController extends AbstractCrudController
@@ -12,14 +15,20 @@ class UserCrudController extends AbstractCrudController
         return User::class;
     }
 
-    /*
+
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
+            TextField::new('firstname', 'Prénom'),
+            TextField::new('lastname', 'Nom'),
+            EmailField::new('email', 'Email'),
+            // Ajouter un champ pour le rôle (utilisation d'une liste déroulante)
+            ChoiceField::new('roles', 'Rôle')
+                ->allowMultipleChoices()
+                ->setChoices([
+                    'Utilisateur' => 'ROLE_USER',
+                    'Administrateur' => 'ROLE_ADMIN',
+                ])
         ];
     }
-    */
 }
